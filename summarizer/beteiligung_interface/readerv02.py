698f60ad13d7920c4f4645ae7bb306a9842fbe8e
Daniel Bremer <daniel@dremerb.de>
2021-02-20 19:52:09 +0100

    added documentation and get_contributions_with_meta function

--- summarizer/beteiligung_interface/readerv02.py
+++ summarizer/beteiligung_interface/readerv02.py
@@ -6,6 +6,11 @@ BASEURL = "https://beteiligung.hamburg/domainmodul-test/drupal/dipas-pds"
 
 
 def get_all_contributions_with_meta(projid):
+    """
+    Gets raw data from contributions with all fields in the "properties" section of a feature
+    :param projid: id of the project containing contributions
+    :return: list of dictionaries containing contribution data
+    """
     response = requests.get(f"{BASEURL}/projects/{projid}/contributions")
     # print(response.json())
 
@@ -17,6 +22,12 @@ def get_all_contributions_with_meta(projid):
 
 
 def get_all_contributions(projid, title=False):
+    """
+    Gets raw contribution contents.
+    :param projid: ID of the project
+    :param title: Switch if contribution title should be prepended to result
+    :return: the contribution's text (with prepended title, if title set True)
+    """
     contribs = get_all_contributions_with_meta(projid)
     returnlist = []
     for c in contribs:
@@ -29,6 +40,12 @@ def get_all_contributions(projid, title=False):
 
 
 def get_all_comments_of_contrib(projid, contribid):
+    """
+    Pulls all comments for a specific contribution
+    :param projid: ID of the project
+    :param contribid: ID of the contribution
+    :return: list of comments (text only)
+    """
     response = requests.get(f"{BASEURL}/projects/{projid}/contributions/{contribid}/comments")
 
     comments = []
@@ -40,6 +57,12 @@ def get_all_comments_of_contrib(projid, contribid):
 
 
 def get_all_commented_contributions(projid, title=False):
+    """
+    Get a list of strings consisting of the comment and appended comments
+    :param projid: ID of the project
+    :param title: Switch if contribution title should be prepended to result
+    :return: String containing contribution and comment text
+    """
     response = requests.get(f"{BASEURL}/projects/{projid}/commentedcontributions")
 
     result = []
@@ -57,6 +80,13 @@ def get_all_commented_contributions(projid, title=False):
 
 
 def get_contribution(projid, contribid, title=False):
+    """
+    Get a single contribution (text only)
+    :param projid: ID of the project
+    :param contribid: ID of the contribution
+    :param title: Switch if contribution title should be prepended to result
+    :return: Text of a contribution
+    """
     response = requests.get(f"{BASEURL}/projects/{projid}/contributions/{contribid}")
 
     j = response.json()["features"][0]["properties"]
@@ -66,8 +96,26 @@ def get_contribution(projid, contribid, title=False):
         return j["contributionContent"]
 
 
-def get_contribution_with_comments(projid, nid):
-    contrib = get_contribution(projid, nid)
+def get_contribution_with_meta(projid, contribid):
+    """
+    Get the raw data for a single contribution, as returned from the API
+    :param projid: ID of the project
+    :param contribid: ID of the contribution
+    :return: Dictionary containing return value from API
+    """
+    response = requests.get(f"{BASEURL}/projects/{projid}/contributions/{contribid}")
+    return response.json()["features"][0]["properties"]
+
+
+def get_contribution_with_comments(projid, nid, title=False):
+    """
+    Get a string containing comment text and appended comment texts
+    :param projid: ID of the project
+    :param nid: ID of the contribution
+    :param title: Switch if contribution title should be prepended to result
+    :return: String consisting of a contribution's text and appended comments
+    """
+    contrib = get_contribution(projid, nid, title)
     comments = get_all_comments_of_contrib(projid, nid)
     comstr = ""
     for com in comments:
@@ -77,5 +125,10 @@ def get_contribution_with_comments(projid, nid):
 
 
 def get_contributions_count(projid):
+    """
+    Count all contributions in a project
+    :param projid: ID of the project
+    :return: int, number of contributions
+    """
     list = get_all_contributions(projid)
     return len(list)
